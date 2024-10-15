a86342789a107d7f2c2989a36854e9194fbc2a59
camrohwer <camrohwer@gmail.com>
2024-10-15 19:58:54 +0000

    pit finding, pq working. Added pitcell struct

--- src/pit_filling.cu
+++ src/pit_filling.cu
@@ -6,12 +6,18 @@
 #include <cstdlib>
 #include <queue>
 #include <vector>
+#include <limits.h>
 
-__global__ void identifyPits(const float* dem, int* pits, int width, int height){
+struct PitCell {
+    float elevation;
+    int index;
+};
+
+__global__ void identifyPits(const float* dem, PitCell* pitCells, int* numPits, int width, int height){
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (x >= width || y >= height) return;    
+    if (x <= 1 || x >= width || y <= 1 || y >= height) return; // skip boundary 
 
     int idx = y * width + x;
     float curElev = dem[idx];
@@ -28,7 +34,7 @@ __global__ void identifyPits(const float* dem, int* pits, int width, int height)
             if (nx >= 0 && nx < width && ny >= 0 && ny < height){
                 //get neightbour index
                 int ni = ny * width + nx;
-                if (dem[ni] < curElev){
+                if (dem[ni] <= curElev){
                     isPit = false;
                     break;
                 }
@@ -39,9 +45,9 @@ __global__ void identifyPits(const float* dem, int* pits, int width, int height)
 
     // if pit, store elev
     if (isPit){
-        pits[idx] = curElev;
-    } else {
-        pits[idx] = -1; //-1 is non pit
+        int count = atomicAdd(numPits, 1);
+        pitCells[count].elevation = curElev;
+        pitCells[count].index = idx;
     }
 }
 int main(int argc, char* argv[]){
@@ -71,22 +77,21 @@ int main(int argc, char* argv[]){
     CPLErr err = demDataset->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, width, height, demData, width, height, GDT_Float32, 0, 0);
     if (err != CE_None){
         std::cerr << "Error reading DEM data: " << CPLGetLastErrorMsg() << std::endl;
+        GDALClose(demDataset);
         return -1;
     }
 
     //allocating device mem
     float *d_dem;
-    int *d_pits;
+    PitCell *d_pitCells;
 
-    cudaMalloc(&d_dem, sizeof(float) * width * height);
     if (cudaMalloc(&d_dem, sizeof(float) * width * height) != cudaSuccess) {
         std::cerr << "Error allocating memory for DEM on device." << std::endl;
         return -1;
     }
 
-    cudaMalloc(&d_pits, sizeof(int) * width * height);
-    if (cudaMalloc(&d_pits, sizeof(int) * width * height) != cudaSuccess) {
-        std::cerr << "Error allocating memory for flow direction on device." << std::endl;
+    if (cudaMalloc(&d_pitCells, sizeof(PitCell) * width * height) != cudaSuccess) {
+        std::cerr << "Error allocating memory for pit cells on device." << std::endl;
         cudaFree(d_dem); // Free already allocated memory
         return -1;
     }
@@ -98,37 +103,47 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
+    int* d_numPits;
+    int numPits = 0;
+
+    cudaMalloc(&d_numPits, sizeof(int));
+    cudaMemcpy(d_numPits, &numPits, sizeof(int), cudaMemcpyHostToDevice);
+
     //define grid and block size
     dim3 blockSize(8,8);
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
 
-    identifyPits<<<gridSize, blockSize>>>(d_dem, d_pits, width, height);
+    identifyPits<<<gridSize, blockSize>>>(d_dem, d_pitCells, d_numPits, width, height);
     cudaDeviceSynchronize();
 
-    //copy pits back to host
-    int size = width * height;
-    std::vector<int> pits(size);
-    cudaMemcpy(pits.data(), d_pits, size * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaError_t kernel_err = cudaGetLastError();
+    if (kernel_err != cudaSuccess){
+        std::cerr << "Kernel Launch failed: " << cudaGetErrorString(kernel_err) << std::endl;
+        return -1;
+    }
 
-    size_t pitsSize = pits.size();
-    std::cout << "Number of pits: " << pitsSize << std::endl;
+    // copy pit count from device to host
+    cudaMemcpy(&numPits, d_numPits, sizeof(int), cudaMemcpyDeviceToHost);
+
+    std::vector<PitCell> hostPits(numPits);
+    cudaMemcpy(hostPits.data(), d_pitCells, numPits * sizeof(PitCell), cudaMemcpyDeviceToHost);
 
     cudaFree(d_dem);
-    cudaFree(d_pits);
+    cudaFree(d_pitCells);
+    cudaFree(d_numPits);
 
     //place pits in PQ
     std::priority_queue<int> pq;
-
-    for (int i = 0; i < size; ++i){
-        if (pits[i] != -1){
-            pq.push(pits[i]);
-        }
+    for (const auto& pit : hostPits){
+        pq.push(pit.elevation);
     }
 
     //OUPUT FOR TESTING
+    int j = 0;
     std::cout << "Pits sorted by elevation" << std::endl;
     while(!pq.empty()){
-        //std::cout << pq.top() << std::endl;
+        j++;
         pq.pop();
     }
+    std::cout << "Size of PQ: " << j <<std::endl;
 }
