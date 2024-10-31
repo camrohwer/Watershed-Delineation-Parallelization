dd2b4e82d151683f76eddf946d85bfa8c821dca3
camrohwer <camrohwer@gmail.com>
2024-10-31 21:26:42 +0000

    fix projection and geotransform for flow direction calculation

--- src/flow_direction_parallel.cu
+++ src/flow_direction_parallel.cu
@@ -65,8 +65,17 @@ int main(int argc, char* argv[]) {
     const char* input = argv[1];
     GDALDataset* demDataset  = (GDALDataset*) GDALOpen(input, GA_ReadOnly);
 
-    if (demDataset == NULL) {
+    if (demDataset == nullptr) {
         std::cerr << "Error opening DEM file." << std::endl;
+        return -1;
+    }
+
+    const char* projection = demDataset->GetProjectionRef();
+    double geoTransform[6];
+
+    if (demDataset->GetGeoTransform(geoTransform) != CE_None){
+        std::cerr << "Error reading geo-transfor" << std::endl;
+        GDALClose(demDataset);
         return -1;
     }
 
@@ -79,6 +88,9 @@ int main(int argc, char* argv[]) {
                                                     demDataset->GetRasterXSize(),
                                                     demDataset->GetRasterYSize(),
                                                     1, GDT_Int32, NULL);
+
+    flowDirDataset->SetProjection(projection);
+    flowDirDataset->SetGeoTransform(geoTransform);
 
     //Raster size to use with Malloc and device mem
     int width = demDataset->GetRasterXSize();
@@ -134,8 +146,7 @@ int main(int argc, char* argv[]) {
     cudaMemcpy(flowDirData, d_flowDirData, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
 
     // Write flow direction data to the output dataset
-    err = flowDirDataset->GetRasterBand(1)->RasterIO(GF_Write, 0, 0, width, height,
-                                                     flowDirData, width, height, GDT_Int32, 0, 0);
+    err = flowDirDataset->GetRasterBand(1)->RasterIO(GF_Write, 0, 0, width, height, flowDirData, width, height, GDT_Int32, 0, 0);
     if (err != CE_None) {
         std::cerr << "Error writing flow direction data: " << CPLGetLastErrorMsg() << std::endl;
         return -1;
@@ -150,6 +161,5 @@ int main(int argc, char* argv[]) {
     GDALClose(flowDirDataset);
 
     std::cout << "Flow direction calculated and saved to " << outputFilename << std::endl;
-
     return 0;
 }
