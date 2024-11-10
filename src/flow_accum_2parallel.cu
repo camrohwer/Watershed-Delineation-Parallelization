af2582bbaaa6469f933a79ece4f861f02bb9016d
ColbyMusialek <69180396+ColbyMusialek@users.noreply.github.com>
2024-11-10 03:11:10 +0000

    not working

--- src/flow_accum_2parallel.cu
+++ src/flow_accum_2parallel.cu
@@ -8,74 +8,62 @@
 #define BLOCK_SIZE 16
 //two nested for loops - handle in the neighborhood sequentially
 //fourth array - takes place of the cells - used for writing final to? cumulative?
-__global__ void flowAccumKernel(int* gpuOldFlow, int* gpuNewFlow, int * flowDir, bool* gpuStop, int N, int M){
+__global__ void flowAccumKernel(int* gpuAccum, int* gpuOldFlow, int* gpuNewFlow, int * flowDir, bool* gpuStop, int N, int M){
     //__shared__ int sharedOldFlow[BLOCK_SIZE][BLOCK_SIZE];
     //*gpuStop = false;
-    int x = blockIdx.x * blockDim.x + threadIdx.x; 
-    int y = blockIdx.y * blockDim.x + threadIdx.y;
+    //int x = blockIdx.x * blockDim.x + threadIdx.x; 
+    //int y = blockIdx.y * blockDim.x + threadIdx.y;
     int i = THREADCELLS * (blockIdx.y * BLOCK_SIZE + threadIdx.y);
     int j = THREADCELLS * (blockIdx.x * BLOCK_SIZE + threadIdx.x);
-    for (int r = i; i <= i + THREADCELLS; i++){
-        for (int s = j; j <= j + THREADCELLS; j++){
-
+    if (i >= N || j >= M) return;
+    for (int r = i; r <= i + THREADCELLS; r++){
+        for (int s = j; s <= j + THREADCELLS; s++){
+            int currFlow = gpuOldFlow[r * M + s];
+            if (currFlow > 0){
+                gpuOldFlow[r * M + s] = 0;
+                int targetX = 0;
+                int targetY = 0;
+                int dir = flowDir[r * M + s];
+                switch (dir){
+                case 1: //northwest
+                    targetY -=1;
+                    targetX -=1;
+                    break;
+                case 2: //north
+                    targetY -=1;
+                    break;
+                case 3: //northeast
+                    targetY -=1;
+                    targetX +=1;
+                    break;
+                case 4: //east
+                    targetX += 1;
+                    break;
+                case 5: //southeast
+                    targetY +=1;
+                    targetX +=1;
+                    break;
+                case 6: //south
+                    targetY +=1;
+                    break;
+                case 7: //southwest
+                    targetY +=1;
+                    targetX -=1;
+                    break;
+                case 8: //west
+                    targetX -=1;
+                    break;
+                }
+                int k = i + targetX;
+                int l = j + targetY;
+                if (k >= 0 && k < N && l >= 0 && l < M) {
+                    atomicAdd(&gpuNewFlow[l * M + k], currFlow);
+                    atomicAdd(&gpuAccum[l * M + k], currFlow);
+                    *gpuStop = true;
+                }
+            } 
         }
     }
-    if (x >= M || y >= N){
-        return;
-    }
-    int idx = y * M + x;
-    //load flow data into shared memory
-    //sharedOldFlow[x][y] = gpuOldFlow[idx];
-    //__syncthreads();
-
-    int flow = gpuOldFlow[y* M + x];
-    //printf("(%d, %d) - %d  ",x,y, flow);
-    if (flow <= 0 || flowDir[idx] == 0) return;
-    if (flow > 0){
-        gpuOldFlow[y* M + x] = 0;
-        int dir = flowDir[idx];
-        int accum = 0;
-        switch (dir){
-            case 1: //northwest
-                targetY -=1;
-                targetX -=1;
-                break;
-            case 2: //north
-                targetY -=1;
-                break;
-            case 3: //northeast
-                targetY -=1;
-                targetX +=1;
-                break;
-            case 4: //east
-                targetX += 1;
-                break;
-            case 5: //southeast
-                targetY +=1;
-                targetX +=1;
-                break;
-            case 6: //south
-                targetY +=1;
-                break;
-            case 7: //southwest
-                targetY +=1;
-                targetX -=1;
-                break;
-            case 8: //west
-                targetX -=1;
-                break;
-        }
-        //printf("--%d, %d--", targetX, targetY);
-
-        if (targetX >= 0 && targetX < M && targetY >= 0 && targetY < N){
-            atomicAdd(&gpuNewFlow[targetY * M + targetX], flow);
-            *gpuStop = true;
-            //printf("adjustment");
-        }
-    }
-
-
-
 }
 
 int main(int argc, char* argv[]){
@@ -112,24 +100,25 @@ int main(int argc, char* argv[]){
     int width = D8Dataset->GetRasterXSize();
     int height = D8Dataset->GetRasterYSize();
     bool *stopFlag = new bool;
-    float *flowDir = (float *)CPLMalloc(sizeof(float) * width * height);
+    int *flowDir = (int *)CPLMalloc(sizeof(int) * width * height);
     
 
     //populate demData dynamically allocated memory
-    CPLErr err = D8Dataset->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, width, height, flowDir, width, height, GDT_Float32, 0, 0);
+    CPLErr err = D8Dataset->GetRasterBand(1)->RasterIO(GF_Read, 0, 0, width, height, flowDir, width, height, GDT_Int32, 0, 0);
     if (err != CE_None){
         std::cerr << "Error reading DEM data: " << CPLGetLastErrorMsg() << std::endl;
         return -1;
     }
 
     //allocating device mem
-    int *d_oldFlow, *d_newFlow, *d_flowDir;
+    int *d_oldFlow, *d_newFlow, *d_flowDir, *d_accum;
     bool *d_stopFlag;
 
     
     cudaMalloc(&d_oldFlow, width * height * sizeof(int));
     cudaMalloc(&d_newFlow, width * height * sizeof(int));
     cudaMalloc(&d_flowDir, width * height * sizeof(int));
+    cudaMalloc(&d_accum, width * height * sizeof(int));
     cudaMalloc(&d_stopFlag, sizeof(bool));
     
     //copy flow direction data to device
@@ -140,11 +129,14 @@ int main(int argc, char* argv[]){
     }
     int* hostOldFlow = new int[width * height];
     for (int i = 0; i < width * height; ++i) {
-        hostOldFlow[i] = 1;
+        //set this to 0 or 1?
+        //was 1 before
+        hostOldFlow[i] = 0;
     }
 
     cudaMemcpy(d_oldFlow, hostOldFlow, width * height * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemset(d_newFlow, 0, sizeof(int) * width *height);
+    cudaMemset(d_accum, 0, sizeof(int) * width * height);   
     //define grid and block size
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
@@ -153,9 +145,15 @@ int main(int argc, char* argv[]){
     //call kernel 
     //do{
     for(int x = 0; x < 3; x++){
+        printf("Iteration: %d\n", x);
         *stopFlag = false;
         cudaMemcpy(d_stopFlag, stopFlag, sizeof(bool), cudaMemcpyHostToDevice);
-        flowAccumKernel<<<gridSize, blockSize>>>(d_oldFlow, d_newFlow, d_flowDir, d_stopFlag, height, width);
+        flowAccumKernel<<<gridSize, blockSize>>>(d_accum, d_oldFlow, d_newFlow, d_flowDir, d_stopFlag, height, width);
+        cudaError_t kernelErr = cudaGetLastError();
+        if (kernelErr != cudaSuccess){
+            std::cerr << "Error launching kernel: " << cudaGetErrorString(kernelErr) << std::endl;
+            return -1;
+        }
         cudaDeviceSynchronize();
         cudaMemcpy(stopFlag, d_stopFlag, sizeof(bool), cudaMemcpyDeviceToHost);
         int *temp = d_oldFlow;
@@ -168,7 +166,7 @@ int main(int argc, char* argv[]){
     }
     int *hostflowAccumulationData = (int *)CPLMalloc(sizeof(int) * width * height);
     cudaDeviceSynchronize();
-    cudaMemcpy(hostflowAccumulationData, d_oldFlow, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
+    cudaMemcpy(hostflowAccumulationData, d_accum, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
     err = flowAccumDataset->GetRasterBand(1)->RasterIO(GF_Write, 0, 0, width, height,
         hostflowAccumulationData, width, height, GDT_Int32, 0, 0);
     if (err != CE_None) {
@@ -181,6 +179,7 @@ int main(int argc, char* argv[]){
     cudaFree(d_newFlow);
     cudaFree(d_flowDir);
     cudaFree(d_stopFlag);
+    cudaFree(d_accum);
     CPLFree(flowDir);
     GDALClose(D8Dataset);
     GDALClose(flowAccumDataset);
