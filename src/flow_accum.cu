1fe9ff27ca43c1b6f306501b0a185c8a97d4865d
camrohwer <camrohwer@gmail.com>
2024-11-12 01:16:37 +0000

    code cleanup

--- src/flow_accum.cu
+++ src/flow_accum.cu
@@ -10,12 +10,12 @@
 
 __constant__ int offsetX[9] = {0, -1, 0, 1, 1, 1, 0, -1, -1};
 __constant__ int offsetY[9] = {0, -1, -1, -1, 0, 1, 1, 1, 0};
-__constant__ int direction[9] = {0, 1, 2, 3, 4, 5, 6, 7, 8}; 
+// direction = {0, 1, 2, 3, 4, 5, 6, 7, 8}; 
 
 
 //two nested for loops - handle in the neighborhood sequentially
 //fourth array - takes place of the cells - used for writing final to? cumulative?
-__global__ void flowAccumKernel(int* gpuAccum, int* gpuOldFlow, int* gpuNewFlow, int * flowDir, bool* gpuStop, int N, int M){
+__global__ void flowAccumKernel(int* gpuAccum, int* gpuOldFlow, int* gpuNewFlow, int * flowDir, int* gpuStop, int N, int M){
     int i = THREADCELLS * (blockIdx.y * BLOCK_SIZE + threadIdx.y);
     int j = THREADCELLS * (blockIdx.x * BLOCK_SIZE + threadIdx.x);
 
@@ -33,6 +33,7 @@ __global__ void flowAccumKernel(int* gpuAccum, int* gpuOldFlow, int* gpuNewFlow,
                 if (newR >= 0 && newR < N && newS >= 0 && newS < M) {
                     atomicAdd(&gpuNewFlow[newR * M + newS], currFlow);
                     atomicAdd(&gpuAccum[newR * M + newS], currFlow);
+                    atomicOr(gpuStop, 1);
                 }
             } 
         }
@@ -40,8 +41,6 @@ __global__ void flowAccumKernel(int* gpuAccum, int* gpuOldFlow, int* gpuNewFlow,
 }
 
 int main(int argc, char* argv[]){
-
-    //checks for input file passed as arg
     if (argc < 3){
         std::cout << "Please provide a filepath for input and output raster" << std::endl;
         return -1;
@@ -60,7 +59,6 @@ int main(int argc, char* argv[]){
 
     //create output raster for flow accumulation
     const char *outputFilename = argv[2];
-
     //Geotiff Driver
     GDALDriver *poDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
     //32int Empty raster with same dims as input
@@ -72,7 +70,6 @@ int main(int argc, char* argv[]){
     //Raster size to use with Malloc and device mem
     int width = D8Dataset->GetRasterXSize();
     int height = D8Dataset->GetRasterYSize();
-    bool *stopFlag = new bool;
     int *flowDir = (int *)CPLMalloc(sizeof(int) * width * height);
 
     //populate demData dynamically allocated memory
@@ -83,8 +80,7 @@ int main(int argc, char* argv[]){
     }
 
     //allocating device mem
-    int *d_oldFlow, *d_newFlow, *d_flowDir, *d_accum;
-    bool *d_stopFlag;
+    int *d_oldFlow, *d_newFlow, *d_flowDir, *d_accum, *d_stopFlag;
     
     // Allocate memory for d_oldFlow on device
     if (cudaMalloc(&d_oldFlow, width * height * sizeof(int)) != cudaSuccess) {
@@ -107,7 +103,7 @@ int main(int argc, char* argv[]){
         return -1;
     }
     // Allocate memory for d_stopFlag on device
-    if (cudaMalloc(&d_stopFlag, sizeof(bool)) != cudaSuccess) {
+    if (cudaMalloc(&d_stopFlag, sizeof(int)) != cudaSuccess) {
         std::cerr << "Error allocating memory for Stop Flag on device" << std::endl;
         return -1;
     }
@@ -119,14 +115,8 @@ int main(int argc, char* argv[]){
         return -1;
     }
     int* hostOldFlow = new int[width * height];
-    for (int i = 0; i < width * height; ++i) {
-        hostOldFlow[i] = 1;
-    }
-
     int* hostNewFlow = new int [width*height];
-    for (int i = 0; i < width * height; ++i) {
-        hostNewFlow[i] = 0;
-    }
+    for (int i = 0; i < width * height; ++i) hostOldFlow[i] = 1, hostNewFlow[i] = 0;
 
     cudaMemcpy(d_oldFlow, hostOldFlow, width * height * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_newFlow, hostNewFlow, width * height * sizeof(int), cudaMemcpyHostToDevice);
@@ -136,11 +126,13 @@ int main(int argc, char* argv[]){
     dim3 blockSize(BLOCK_SIZE, BLOCK_SIZE);
     dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
 
-    //do{
-    for(int x = 0; x < 3; x++){
-        printf("Iteration: %d\n", x);
-        *stopFlag = false;
-        cudaMemcpy(d_stopFlag, stopFlag, sizeof(bool), cudaMemcpyHostToDevice);
+    int x = 0;
+    int *stopFlag = new int(0);
+    do{
+        printf("Iteration: %d\n", x++);
+        *stopFlag = 0;
+        cudaMemcpy(d_stopFlag, stopFlag, sizeof(int), cudaMemcpyHostToDevice);
+
         flowAccumKernel<<<gridSize, blockSize>>>(d_accum, d_oldFlow, d_newFlow, d_flowDir, d_stopFlag, height, width);
         cudaError_t kernelErr = cudaGetLastError();
         if (kernelErr != cudaSuccess){
@@ -148,18 +140,18 @@ int main(int argc, char* argv[]){
             return -1;
         }
         cudaDeviceSynchronize();
-        cudaMemcpy(stopFlag, d_stopFlag, sizeof(bool), cudaMemcpyDeviceToHost);
+        cudaMemcpy(stopFlag, d_stopFlag, sizeof(int), cudaMemcpyDeviceToHost);
+
         int *temp = d_oldFlow;
         d_oldFlow = d_newFlow;
         d_newFlow = temp;
-
         cudaMemset(d_newFlow, 0, sizeof(int) * width * height);
-        
-    //} while (*stopFlag);
-    }
+    } while (*stopFlag != 0);
+
     int *hostflowAccumulationData = (int *)CPLMalloc(sizeof(int) * width * height);
     cudaDeviceSynchronize();
     cudaMemcpy(hostflowAccumulationData, d_accum, sizeof(int) * width * height, cudaMemcpyDeviceToHost);
+
     err = flowAccumDataset->GetRasterBand(1)->RasterIO(GF_Write, 0, 0, width, height,
         hostflowAccumulationData, width, height, GDT_Int32, 0, 0);
     if (err != CE_None) {
@@ -167,13 +159,10 @@ int main(int argc, char* argv[]){
         return -1;
     }
     
-    cudaFree(d_oldFlow);
-    cudaFree(d_newFlow);
-    cudaFree(d_flowDir);
-    cudaFree(d_stopFlag);
-    cudaFree(d_accum);
+    cudaFree(d_oldFlow); cudaFree(d_newFlow); cudaFree(d_flowDir);
+    cudaFree(d_stopFlag); cudaFree(d_accum);
     CPLFree(flowDir);
-    GDALClose(D8Dataset);
-    GDALClose(flowAccumDataset);
+    GDALClose(D8Dataset); GDALClose(flowAccumDataset);
+    delete[] hostOldFlow; delete[] hostNewFlow;
     return 0;
 }
